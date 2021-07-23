f382aa5cb17978e270c00e7a776da95111b3a1bf
Vutran2211 <tranvu0905.lsr@gmail.com>
2021-07-23 23:36:41 +0700

    sua bai 3 bai 4

--- bai3.cpp
+++ bai3.cpp
@@ -2,24 +2,18 @@
 #include <math.h>
 
 using namespace std;
+long long n, a[100005]={0};
 
 int main(){
-	int n,k;
+	long long x, k, count=0;
 	cin>>n>>k;
-	long long a[n];
-	for(int i=1; i<=n; ++i)
-	cin>>a[i];
-	
-	int i=n,count=-1;
-	for(int i=1; i<=n; i++){
-	 	if(a[i]%k != a[i+1]%k){
-	 		count++;
-		 	}
-		if(a[i]%k == a[i+1]%k){
-			count = 0;
+	for(long long i=1;i<n;i++){
+		cin>>x;
+		if(a[x%k]==0){
+			count++;
+			a[x%k]=1;
 			}
-	 	}
+		}
 	cout<<count;
-	return 0;
 }
 
--- bai4.cpp
+++ bai4.cpp
@@ -2,21 +2,51 @@
 #include <math.h>
 
 using namespace std;
+ 
+int n,k,count=0,a[100001];
 
-int main(){
-	int n,k;
+void Enter()
+{
 	cin>>n>>k;
-	long long a[n];
-	for(int i=1; i<=n; ++i)
-	cin>>a[i];
-	
-	int count=0;
+	for (int i=1;i<=n;i++)
+		cin>>a[i];
+}
+ 
+void QuickSort(int a[],int L,int H)
+{
+	int i=L,j=H,tam;
+	int mid= a[(L+H)/2];
+	while (i<j)
+	{
+		while (a[i]<mid) i++;
+		while (a[j]>mid) j--;
+		if (i<=j)
+		{
+		   tam=a[i];
+		   a[i]=a[j];
+		   a[j]=tam;
+		   i++;
+		   j--;	
+		}
+	}
+	if (L<j) QuickSort(a,L,j);
+	if (i<H) QuickSort(a,i,H);	
+}
+ 
+int Optimize()
+{
 	for (int i=1;i<n;i++)
 		for (int j=i+1;j<=n;j++)
 		{
 			if (a[j]-a[i]==k) count++;
 			if (a[j]-a[i]>k) break;
 		}
-		cout<<count;
-		return 0;
-	}
+	return ans;
+}
+ 
+int main()
+{
+	Enter();
+	QuickSort(a,1,n);
+	cout<<Optimize();
+}

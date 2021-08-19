4238426313c71c6f4d1968ff21e600c09dc67879
Vutran2211 <tranvu0905.lsr@gmail.com>
2021-08-19 22:29:57 +0700

    bai14fix

--- bai14.cpp
+++ bai14.cpp
@@ -4,17 +4,16 @@
 using namespace std;
 
 int main(){
-	int n;
-	int a[100];
+	long long n;
+	long long a[10001];
 	cin>>n;
 	for(int i=0; i<n; i++){
 		cin>>a[i];
 	}
     sort(a,a+n);// xap xep mang theo thu tu tang dan
-
-    int j=1;
-        while(j!=0){
-            int b = j*j;
+    int j=0;
+        while(j>=0){
+            long long b = j*j;
             int i;
             for(i=0;i<sizeof(a)/sizeof(a[0]);i++){
                 if(b==a[i]){
@@ -25,12 +24,15 @@ int main(){
                     i=sizeof(a)/sizeof(a[0]);//de so sanh o duoi
                     break;
                 }
+                if(b!=0 && b<=a[i]){
+                	cout<<0;
+                }
             }
             if(i==sizeof(a)/sizeof(a[0])){
                 cout<<b;
-                j=0;//de thoat vong lap while
+                j=-1;//de thoat vong lap while
             }else{
-                j++;//tiep t?c vong lap while
+                j++;//tiep tuc vong lap while
             }
         }
 }
